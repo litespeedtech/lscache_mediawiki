9d4b9c7e388add0e6bf96f9f8be09048e5cd473e
Wuhua Chen <wchen@litespeedtech.com>
2018-05-30 12:02:49 -0400

    [Bugfix] add default argument value to prevent invalid argument number problem

--- LiteSpeedCache/LiteSpeedCache_body.php
+++ LiteSpeedCache/LiteSpeedCache_body.php
@@ -114,7 +114,7 @@ class LiteSpeedCache
      *
      * @since   1.0.1
      */
-    public static function onPageContentSaveComplete($article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId, $undidRevId)
+    public static function onPageContentSaveComplete($article, $user, $content, $summary=null, $isMinor=null, $isWatch=null, $section=null, $flags=null, $revision=null, $status=null, $baseRevId=null, $undidRevId=null )
     {
         if (!self::isCacheEnabled()) {
             return;
